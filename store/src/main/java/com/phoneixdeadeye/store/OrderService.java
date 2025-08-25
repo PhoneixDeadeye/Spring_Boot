cd44d81b196cd2b57344ecd59e61d756b17fff8b
Rohan Agarwal <agarwalrohan2004@gmail.com>
2025-08-25 13:07:43 +0530

    Added OrderService with PaymentService integration

---
+++ store/src/main/java/com/phoneixdeadeye/store/OrderService.java
@@ -0,0 +1,21 @@
+package com.phoneixdeadeye.store;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+    private PaymentService paymentService;
+
+    public OrderService(PaymentService paymentService){
+        this.paymentService = paymentService;
+    }
+
+    public void placeOrder() {
+        paymentService.processPayment(100.0);
+    }
+
+    public void setPaymentService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+}
